263ed60dba11f00c731ba37422f66032c6cca76c
Arseny <116286402+ArsenySlow@users.noreply.github.com>
2024-11-11 18:12:03 +0400

    ts file eddited

---
+++ 1/ts/index.ts
@@ -0,0 +1,29 @@
+//node .\index.ts для запуска тайпскпипт файла
+//tsc index.ts для компиляции ts файла в js
+
+
+interface Users{
+    name: string,
+    age: number
+}
+
+const firstPerson : Users ={
+    name:"Ola",
+        age:15
+}
+
+
+
+const myNumber =3;
+
+let myNumber2 = 'rty'
+const one = 1 ;
+const two = 2 ;
+const summ = one + two; // + - / * %
+const summ2 = '';
+console.log ('Значение =',summ);
+console.log(myNumber)
+console.log(myNumber2)
+console.log(typeof myNumber)
+console.log(typeof myNumber2)
+console.log(typeof summ)
